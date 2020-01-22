eea3eef76cc020df3fe3aff6109a42d86801e44e
Brandolini <revitoop@gmail.com>
2020-01-22 16:43:36 -0600

    Added Router Functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React from 'react';
+import { Route, Switch, Link } from 'react-router-dom';
 
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+)
+const HatsDetailPage = (props) => (
+  <div>
+    <h1>HATS DETAIL PAGE</h1>
+    <Link to='/'>Back To Home</Link>
+    <button onClick={() => props.history.push('/')}>Home</button>
+  </div>
+)
 function App() {
   return (
     <div>
-      <HomePage></HomePage>
+      <Switch>
+        <Route exact path='/' component={HomePage}></Route>
+        <Route exact path='/shop/hats' component={HatsPage}></Route>
+        <Route path='/hats/:hatsId' component={HatsDetailPage}></Route>
+      </Switch>
     </div>
   );
 }
