c4da7c3cc19c605e30732ef6349288e8365253e9
Brandolini <revitoop@gmail.com>
2020-01-22 21:35:30 -0600

    Added header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import React from 'react';
-import { Route, Switch, Link } from 'react-router-dom';
+import { Route, Switch} from 'react-router-dom';
 
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-)
-// const HatsDetailPage = (props) => (
-//   <div>
-//     <h1>HATS DETAIL PAGE</h1>
-//     <Link to='/'>Back To Home</Link>
-//     <button onClick={() => props.history.push('/')}>Home</button>
-//   </div>
-// )
+
 function App() {
   return (
     <div>
+    <Header></Header>
       <Switch>
         <Route exact path='/' component={HomePage}></Route>
         <Route exact path='/shop' component={ShopPage}></Route>
