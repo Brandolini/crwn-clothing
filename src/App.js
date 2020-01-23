4a9d75e6d8d7bad34e20d1df60ee467c1812959c
Brandolini <revitoop@gmail.com>
2020-01-23 03:56:35 -0600

    Added Sign In Form with Custom inputs and buttons

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 
 function App() {
@@ -14,7 +15,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage}></Route>
         <Route exact path='/shop' component={ShopPage}></Route>
-        
+        <Route exact path='/signin' component={SignInAndSignUp}></Route>
       </Switch>
     </div>
   );
