cc59fee9a58d5528b80cc85a7f07cbc75001fefa
Brandolini <revitoop@gmail.com>
2020-02-06 15:06:15 -0600

    Added styled-components styling to components

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -7,7 +7,7 @@ import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 import { selectCartHidden } from '../../redux/cart/cart.selectors';
 import { selectCurrentUser } from '../../redux/user/user.selectors';
-import { HeaderContainer, LogoContainer, OptionsContainer, OptionDiv, OptionLink }from './header.styles';
+import { HeaderContainer, LogoContainer, OptionsContainer, OptionLink }from './header.styles';
 
 const Header = ({currentUser, hidden}) => (
     <HeaderContainer>
@@ -19,8 +19,9 @@ const Header = ({currentUser, hidden}) => (
             <OptionLink to="/contact">CONTACT</OptionLink>
             {
                 currentUser ?
-                <OptionDiv 
-                    onClick={() => auth.signOut()}> SIGN OUT</OptionDiv>
+                <OptionLink
+                    as='div' 
+                    onClick={() => auth.signOut()}> SIGN OUT</OptionLink>
                 :
                 <OptionLink to="/signin">SIGN IN</OptionLink>
             }
